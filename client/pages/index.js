b4a3067e01a6aeb13921366082f406905d19a3af
Yang Lu <yang.youngsoul@gmail.com>
2020-10-08 15:23:59 +0800

    cross namespace service communication

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,13 @@
-const Index = () => {
-  return <div>Landing Page</div>;
+import buildClient from '../api/build-client';
+
+const Index = ({ currentUser }) => {
+  console.log(currentUser);
+  return <div>Landing Page </div>;
+};
+
+Index.getInitialProps = async (context) => {
+  const response = await buildClient(context).get('/api/users/currentuser');
+  return response.data;
 };
 
 export default Index;
