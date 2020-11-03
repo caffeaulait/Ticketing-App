a24597f4984fc982530dadff187943c354dd81fb
Yang Lu <yang.youngsoul@gmail.com>
2020-11-04 00:56:02 +0800

    update client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,40 @@
+const { default: Link } = require('next/link');
 
-
-const Index = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in </h1>
-  ) : (
-    <h1>Please Sign in first</h1>
+const Index = ({ currentUser, tickets }) => {
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tickets.map((el) => {
+            return (
+              <tr key={el.id}>
+                <td>{el.title}</td>
+                <td>{el.price}</td>
+                <td>
+                  <Link href='tickets/[ticketId]' as={`/tickets/${el.id}`}>
+                    <a>View</a>
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-Index.getInitialProps = async (context) => {
-  return {};
+Index.getInitialProps = async (context, client, currentUser) => {
+  const { data } = await client.get('/api/tickets');
+  return { tickets: data };
 };
 
 export default Index;
